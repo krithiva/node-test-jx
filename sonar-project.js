3da05763073f51be1d37274ec23027cfba749c19
Gogs <gogs@fake.local>
2019-03-06 16:43:32 +0530

    added docker push for gcp

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://35.188.108.66:9000',
+       serverUrl: 'http://35.202.9.226:9000',
        options : {
        'sonar.sources': '.',
        'sonar.inclusions' : 'packages/core/src/**' // Entry point of your code
